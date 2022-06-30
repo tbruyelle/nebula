92574e0edb34300b7d9ffbd6187ebdcf97bd22e5
Thomas Bruyelle <thomas.bruyelle@tendermint.com>
2022-06-30 12:23:59 +0200

    soul #3 createdAt field

--- x/sbt/keeper/msg_server_create_soul.go
+++ x/sbt/keeper/msg_server_create_soul.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/tbruyelle/nebula/x/sbt/types"
@@ -13,6 +14,7 @@ func (k msgServer) CreateSoul(goCtx context.Context, msg *types.MsgCreateSoul) (
 		Creator:     msg.Creator,
 		Name:        msg.Name,
 		Description: msg.Description,
+		CreatedAt:   time.Now().Unix(),
 	}
 	id := k.AppendSoul(ctx, soul)
 	return &types.MsgCreateSoulResponse{Id: id}, nil
